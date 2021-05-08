038a074a8010dffa68c46b01741b8c7f58bb0d23
kdbh2 <quocpp77@gmail.com>
2021-05-08 17:30:23 +0700

    new file:   src/app/components/vattu/tonkho/tonkho.component.ts

---
+++ src/app/components/vattu/tonkho/tonkho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vattu-tonkho',
+  templateUrl: './tonkho.component.html',
+  styleUrls: ['./tonkho.component.scss']
+})
+export class TonkhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
